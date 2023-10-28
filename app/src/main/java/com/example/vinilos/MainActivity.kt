ee8fcb86d62486d8f6dfececfc81b709da2be914
lacortesz <la.cortesz1@uniandes.edu.co>
2023-10-28 13:39:51 -0500

    HU02-01: Recuperacion un album por Id

--- app/src/main/java/com/example/vinilos/MainActivity.kt
+++ app/src/main/java/com/example/vinilos/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import com.android.volley.Response
 import com.example.vinilos.broker.VolleyBroker
@@ -22,12 +23,16 @@ class MainActivity : AppCompatActivity() {
         val getButton: Button = findViewById(R.id.get_albums_button)
         val getResultTextView : TextView = findViewById(R.id.get_result_text)
         getButton.setOnClickListener {
-            volleyBroker.instance.add(VolleyBroker.getRequest("albums",
-                Response.Listener<String> { response ->
+
+            val idAlbum: EditText = findViewById(R.id.id_album);
+            val album = idAlbum.text
+
+            volleyBroker.instance.add(VolleyBroker.getRequest("albums/" + album,
+                { response ->
                     // Display the first 500 characters of the response string.
                     getResultTextView.text = "Response is: ${response}"
                 },
-                Response.ErrorListener {
+                {
                     Log.d("TAG", it.toString())
                     getResultTextView.text = it.toString()
                 }
