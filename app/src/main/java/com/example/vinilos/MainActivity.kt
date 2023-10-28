7a3854067b43264a115c9c534f4dd6169bc5a31f
lacortesz <la.cortesz1@uniandes.edu.co>
2023-10-28 13:07:37 -0500

    HU01-01: Recuperacion informacion albumes

--- app/src/main/java/com/example/vinilos/MainActivity.kt
+++ app/src/main/java/com/example/vinilos/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.vinilos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import com.android.volley.Response
+import com.example.vinilos.broker.VolleyBroker
+
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var volleyBroker: VolleyBroker
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        volleyBroker = VolleyBroker(this.applicationContext)
+
+        val getButton: Button = findViewById(R.id.get_albums_button)
+        val getResultTextView : TextView = findViewById(R.id.get_result_text)
+        getButton.setOnClickListener {
+            volleyBroker.instance.add(VolleyBroker.getRequest("albums",
+                Response.Listener<String> { response ->
+                    // Display the first 500 characters of the response string.
+                    getResultTextView.text = "Response is: ${response}"
+                },
+                Response.ErrorListener {
+                    Log.d("TAG", it.toString())
+                    getResultTextView.text = it.toString()
+                }
+            ))
+        }
     }
 }
