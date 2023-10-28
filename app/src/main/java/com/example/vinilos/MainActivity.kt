a1503938635b3f9c2c053fe098928688a7ba52a0
lacortesz <la.cortesz1@uniandes.edu.co>
2023-10-28 14:18:45 -0500

    HU07-01: Avance request creacion album

--- app/src/main/java/com/example/vinilos/MainActivity.kt
+++ app/src/main/java/com/example/vinilos/MainActivity.kt
@@ -6,9 +6,10 @@ import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import com.android.volley.Response
 import com.example.vinilos.broker.VolleyBroker
-
-
+import com.google.android.material.textfield.TextInputEditText
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
     lateinit var volleyBroker: VolleyBroker
@@ -19,21 +20,32 @@ class MainActivity : AppCompatActivity() {
 
         volleyBroker = VolleyBroker(this.applicationContext)
 
-        val getButton: Button = findViewById(R.id.get_albums_button)
-        val getResultTextView : TextView = findViewById(R.id.get_result_text)
-        getButton.setOnClickListener {
+        val postButton: Button = findViewById(R.id.post_album_button)
+        val postResultTextView : TextView = findViewById(R.id.get_result_text)
+        postButton.setOnClickListener {
 
-            val idAlbum: EditText = findViewById(R.id.id_album)
-            val album = idAlbum.text
-
-            volleyBroker.instance.add(VolleyBroker.getRequest("albums/" + album,
-                { response ->
+            val nameTxt: EditText = findViewById(R.id.album_nombre)
+            val coverTxt: EditText = findViewById(R.id.album_cover)
+            val fechaLanzamientoTxt: EditText = findViewById(R.id.album_fecha_lanzamiento)
+            val descripcionTxt: EditText = findViewById(R.id.album_descripcion)
+            val generoTxt: EditText = findViewById(R.id.album_genero)
+            val disqueraTxt: EditText = findViewById(R.id.album_disquera)
+            val postParams = mapOf<String, Any>(
+                "nombre" to nameTxt.text.toString(),
+                "cover" to coverTxt.text.toString(),
+                "fechaLanzamiento" to fechaLanzamientoTxt.text.toString(),
+                "descripcion" to descripcionTxt.text.toString(),
+                "genero" to generoTxt.text.toString(),
+                "disquera" to disqueraTxt.text.toString()
+            )
+            volleyBroker.instance.add(VolleyBroker.postRequest("albums", JSONObject(postParams),
+                Response.Listener<JSONObject> { response ->
                     // Display the first 500 characters of the response string.
-                    getResultTextView.text = "Response is: ${response}"
+                    postResultTextView.text = "Response is: ${response.toString()}"
                 },
-                {
+                Response.ErrorListener {
                     Log.d("TAG", it.toString())
-                    getResultTextView.text = it.toString()
+                    postResultTextView.text = "That didn't work!"
                 }
             ))
         }
