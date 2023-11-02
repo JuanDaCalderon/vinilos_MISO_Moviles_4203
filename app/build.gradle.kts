5da3b44daa42adebdae56b840e5fe3bd14212652
lacortesz <la.cortesz1@uniandes.edu.co>
2023-11-02 18:38:50 -0500

    HU07-01: inclusion menu

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.3.5")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.3.5")
 
     implementation ("com.android.volley:volley:1.2.1")   //Inclusion libreria volley para consultas REST
 
