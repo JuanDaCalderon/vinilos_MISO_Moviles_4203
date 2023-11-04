0b87a1e4e18bc635ec9ca00911ccbf7da1605ed0
Leonardo Barrios Poveda <o.barriosp@uniandes.edu.co>
2023-11-04 17:59:48 -0500

    Se incluyen casos de prueba con Espresso para listar albumes y detalle album

---
+++ app/src/androidTest/java/com/example/vinilos/ui/ListarAlbumesTest.kt
@@ -0,0 +1,256 @@
+package com.example.vinilos.ui
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.*
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.example.vinilos.R
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.hamcrest.core.IsInstanceOf
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class ListarAlbumesTest {
+
+    @Rule
+    @JvmField
+    var mActivityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun listarAlbumesTest() {
+        Thread.sleep(1000)
+        val bottomNavigationItemView = onView(
+            allOf(
+                withId(R.id.coleccionistasFragment), withContentDescription("Tracks"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.nav_view),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        bottomNavigationItemView.perform(click())
+        Thread.sleep(1000)
+
+        val textView = onView(
+            allOf(
+                withId(com.google.android.material.R.id.navigation_bar_item_large_label_view),
+                withText("Tracks"),
+                withParent(
+                    allOf(
+                        withId(com.google.android.material.R.id.navigation_bar_item_labels_group),
+                        withParent(
+                            allOf(
+                                withId(R.id.coleccionistasFragment),
+                                withContentDescription("Tracks")
+                            )
+                        )
+                    )
+                ),
+                isDisplayed()
+            )
+        )
+        textView.check(matches(withText("Tracks")))
+
+        val frameLayout = onView(
+            allOf(
+                withId(R.id.coleccionistasFragment), withContentDescription("Tracks"),
+                withParent(withParent(withId(R.id.nav_view))),
+                isDisplayed()
+            )
+        )
+        frameLayout.check(matches(isDisplayed()))
+
+        val bottomNavigationItemView2 = onView(
+            allOf(
+                withId(R.id.collectorFragment), withContentDescription("Coleccionistas"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.nav_view),
+                        0
+                    ),
+                    3
+                ),
+                isDisplayed()
+            )
+        )
+        bottomNavigationItemView2.perform(click())
+        Thread.sleep(1000)
+
+        val textView2 = onView(
+            allOf(
+                withId(com.google.android.material.R.id.navigation_bar_item_large_label_view),
+                withText("Coleccionistas"),
+                withParent(
+                    allOf(
+                        withId(com.google.android.material.R.id.navigation_bar_item_labels_group),
+                        withParent(
+                            allOf(
+                                withId(R.id.collectorFragment),
+                                withContentDescription("Coleccionistas")
+                            )
+                        )
+                    )
+                ),
+                isDisplayed()
+            )
+        )
+        textView2.check(matches(withText("Coleccionistas")))
+
+        val frameLayout2 = onView(
+            allOf(
+                withId(R.id.collectorFragment), withContentDescription("Coleccionistas"),
+                withParent(withParent(withId(R.id.nav_view))),
+                isDisplayed()
+            )
+        )
+        frameLayout2.check(matches(isDisplayed()))
+
+        val bottomNavigationItemView3 = onView(
+            allOf(
+                withId(R.id.artistasFragment), withContentDescription("Artistas"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.nav_view),
+                        0
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        bottomNavigationItemView3.perform(click())
+        Thread.sleep(1000)
+
+        val textView3 = onView(
+            allOf(
+                withId(com.google.android.material.R.id.navigation_bar_item_large_label_view),
+                withText("Artistas"),
+                withParent(
+                    allOf(
+                        withId(com.google.android.material.R.id.navigation_bar_item_labels_group),
+                        withParent(
+                            allOf(
+                                withId(R.id.artistasFragment),
+                                withContentDescription("Artistas")
+                            )
+                        )
+                    )
+                ),
+                isDisplayed()
+            )
+        )
+        textView3.check(matches(withText("Artistas")))
+
+        val frameLayout3 = onView(
+            allOf(
+                withId(R.id.artistasFragment), withContentDescription("Artistas"),
+                withParent(withParent(withId(R.id.nav_view))),
+                isDisplayed()
+            )
+        )
+        frameLayout3.check(matches(isDisplayed()))
+
+        val bottomNavigationItemView4 = onView(
+            allOf(
+                withId(R.id.albumFragment), withContentDescription("Albums"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.nav_view),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        bottomNavigationItemView4.perform(click())
+        Thread.sleep(1000)
+
+        val textView4 = onView(
+            allOf(
+                withId(com.google.android.material.R.id.navigation_bar_item_large_label_view),
+                withText("Albums"),
+                withParent(
+                    allOf(
+                        withId(com.google.android.material.R.id.navigation_bar_item_labels_group),
+                        withParent(
+                            allOf(
+                                withId(R.id.albumFragment),
+                                withContentDescription("Albums")
+                            )
+                        )
+                    )
+                ),
+                isDisplayed()
+            )
+        )
+        textView4.check(matches(withText("Albums")))
+
+        val frameLayout4 = onView(
+            allOf(
+                withId(R.id.albumFragment), withContentDescription("Albums"),
+                withParent(withParent(withId(R.id.nav_view))),
+                isDisplayed()
+            )
+        )
+        frameLayout4.check(matches(isDisplayed()))
+
+        val textView5 = onView(
+            allOf(
+                withText("Albums"),
+                withParent(
+                    allOf(
+                        withId(R.id.my_toolbar),
+                        withParent(IsInstanceOf.instanceOf(android.widget.LinearLayout::class.java))
+                    )
+                ),
+                isDisplayed()
+            )
+        )
+        textView5.check(matches(withText("Albums")))
+
+        val imageButton = onView(
+            allOf(
+                withId(R.id.crearAlbumFloatingButton), withContentDescription("Crear album"),
+                withParent(withParent(withId(R.id.nav_host_fragment))),
+                isDisplayed()
+            )
+        )
+        imageButton.check(matches(isDisplayed()))
+        Thread.sleep(3000)
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
