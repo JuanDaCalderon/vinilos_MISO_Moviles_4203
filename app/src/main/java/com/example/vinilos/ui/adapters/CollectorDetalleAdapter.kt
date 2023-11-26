cc26fb7c8ee7b5d5509a25e2785823f880435882
lacortesz <la.cortesz1@uniandes.edu.co>
2023-11-25 20:57:30 -0500

    HU06-01: Ajuste actualizacion imagen artista

--- app/src/main/java/com/example/vinilos/ui/adapters/CollectorDetalleAdapter.kt
+++ app/src/main/java/com/example/vinilos/ui/adapters/CollectorDetalleAdapter.kt
@@ -6,7 +6,6 @@ import android.view.ViewGroup
 import androidx.annotation.LayoutRes
 import androidx.core.net.toUri
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
@@ -14,7 +13,6 @@ import com.bumptech.glide.request.RequestOptions
 import com.example.vinilos.R
 import com.example.vinilos.databinding.ArtistasItemBinding
 import com.example.vinilos.models.Artista
-import com.example.vinilos.ui.ArtistasFragmentDirections
 
 class CollectorDetalleAdapter : RecyclerView.Adapter<CollectorDetalleAdapter.CollectorDetalleViewHolder>(){
 
@@ -38,6 +36,7 @@ class CollectorDetalleAdapter : RecyclerView.Adapter<CollectorDetalleAdapter.Col
         holder.viewDataBinding.also {
             it.artista = artistas[position]
         }
+        holder.bind(artistas[position])
     }
 
     override fun getItemCount(): Int {
